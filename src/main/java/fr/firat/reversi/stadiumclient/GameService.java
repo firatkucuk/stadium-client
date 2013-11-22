f1a88e315e40c1e005911bd9f7e4652061a16236
Fırat KÜÇÜK <firatkucuk@gmail.com>
2013-11-22 13:44:18 +0200

    initial commit

---
+++ src/main/java/fr/firat/reversi/stadiumclient/GameService.java
@@ -0,0 +1,48 @@
+
+package fr.firat.reversi.stadiumclient;
+
+import fr.firat.reversi.stadiumclient.models.GameStatus;
+import fr.firat.reversi.stadiumclient.models.ReversiGameStatus;
+import javax.ws.rs.*;
+
+
+
+public interface GameService {
+
+
+
+    //~ --- [METHODS] --------------------------------------------------------------------------------------------------
+
+    @DELETE
+    @Path("/cancel/{cancellationCode}")
+    @Produces("application/json")
+    public ReversiGameStatus cancelGame(@PathParam("cancellationCode") String cancellationCode);
+
+
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    @GET
+    @Path("/status")
+    @Produces("application/json")
+    public ReversiGameStatus getGameStatus();
+
+
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    @PUT
+    @Path("/move/{authCode}/{piece}")
+    @Produces("application/json")
+    public ReversiGameStatus movePiece(@PathParam("authCode") String authCode,
+            @PathParam("piece") String piece);
+
+
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    @POST
+    @Path("/start")
+    @Produces("application/json")
+    public GameStatus startGame();
+}
---
+++ src/main/java/fr/firat/reversi/stadiumclient/Main.java
@@ -0,0 +1,117 @@
+package fr.firat.reversi.stadiumclient;
+
+import fr.firat.reversi.stadiumclient.models.ReversiGameStatus;
+import java.util.*;
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+
+
+
+public class Main {
+
+
+
+    //~ --- [INSTANCE FIELDS] ------------------------------------------------------------------------------------------
+
+    private final Timer timer = new Timer();
+
+
+
+    //~ --- [CONSTRUCTORS] ---------------------------------------------------------------------------------------------
+
+    public Main(String serverUrl, String authCode, int player) {
+
+        TimerTask pollerTask = new PollerTask(serverUrl, authCode, player);
+
+        timer.scheduleAtFixedRate(pollerTask, 5000, 5000);
+    }
+
+
+
+    //~ --- [METHODS] --------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        String serverUrl = System.getenv("RS_SERVER_URL");
+
+        if (serverUrl == null || serverUrl.isEmpty()) {
+
+            serverUrl = "http://localhost:8080/reversi-stadium/";
+        }
+
+        String authCode = System.getenv("RS_AUTH_CODE");
+
+        if (authCode == null || authCode.isEmpty()) {
+
+            System.err.println("No authentication code for current player found");
+
+            return;
+        }
+
+        String player = System.getenv("RS_PLAYER");
+
+        if (player == null || player.isEmpty()) {
+            System.err.println("No player environmental variable defined.");
+
+            return;
+        } else if (!player.equals("1") && !player.equals("2")) {
+            System.err.println("Player variable should be 1 or 2.");
+
+            return;
+        }
+
+        new Main(serverUrl, authCode, Integer.parseInt(player));
+    }
+
+
+
+    //~ --- [INNER CLASSES] --------------------------------------------------------------------------------------------
+
+    private class PollerTask extends TimerTask {
+
+
+
+        //~ --- [INSTANCE FIELDS] --------------------------------------------------------------------------------------
+
+        private String      authCode;
+        private int         player;
+        private GameService service;
+
+
+
+        //~ --- [CONSTRUCTORS] -----------------------------------------------------------------------------------------
+
+        private PollerTask(String serverUrl, String authCode, int player) {
+
+            this.authCode = authCode;
+            this.player   = player;
+
+            List<JacksonJsonProvider> providers = Arrays.asList(new JacksonJsonProvider());
+            service = JAXRSClientFactory.create(serverUrl, GameService.class, providers);
+        }
+
+
+
+        //~ --- [METHODS] ----------------------------------------------------------------------------------------------
+
+        @Override
+        public void run() {
+
+            ReversiGameStatus reversiGameStatus = service.getGameStatus();
+
+            if (reversiGameStatus.isCancelled()) {
+                timer.cancel();
+            } else if (!reversiGameStatus.isStarted()) {
+                timer.cancel();
+            } else if (reversiGameStatus.getCurrentPlayer() == player) {
+
+                List<String> availableMoves = reversiGameStatus.getAvailableMoves();
+                Random       random         = new Random();
+                int          randomInt      = random.nextInt(availableMoves.size());
+                String       nextMove       = availableMoves.get(randomInt);
+
+                service.movePiece(authCode, nextMove);
+            }
+        }
+    }
+}
